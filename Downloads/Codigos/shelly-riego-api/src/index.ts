d5a837410ed2782d572c968a190926b1d3ca6ce8
IsaacToledo123 <223245@ids.upchiapas.edu.mx>
2024-07-23 04:46:26 -0600

    hoal

--- Downloads/Codigos/shelly-riego-api/src/index.ts
+++ Downloads/Codigos/shelly-riego-api/src/index.ts
@@ -1,38 +1,16 @@
-// import express from 'express';
-// import cors from 'cors';
-// import { CONFIG } from './config/config';
-// import { controlDeviceOutput } from './Api';
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.post('/api/riego', async (req, res) => {
-//   const { state } = req.body;
-//   try {
-//     const response = await controlDeviceOutput(state);
-//     res.json(response);
-//   } catch (error) {
-//     console.error('Error al controlar el riego:', error);
-//     res.status(500).json({ error: 'Error al controlar el riego' });
-//   }
-// });
-
-// app.listen(CONFIG.port, () => {
-//   console.log(`Servidor proxy corriendo en http://localhost:${CONFIG.port}`);
-// });
-
 import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
-dotenv.config();
 import fs from 'fs';
 import https from 'https';
+
+dotenv.config();
 import { CONFIG } from './config/config';
-import { controlDeviceOutput } from './Api';
+
+const app = express();
+const port = process.env.PORT || CONFIG.port;  // Usa CONFIG.port o el puerto de entorno
+const SHELLY_IP = process.env.SHELLY_IP || '192.168.1.100'; // Asegúrate de configurar esto en tu archivo .env
 
 // Load SSL certificates
 const sslOptions = {
@@ -40,11 +18,13 @@ const sslOptions = {
   cert: fs.readFileSync('/etc/letsencrypt/live/she.soursop.lat/fullchain.pem')
 };
 
-const app = express();
-const port = process.env.PORT || 3080;
-const SHELLY_IP = process.env.SHELLY_IP || '192.168.1.100'; // Asegúrate de configurar esto en tu archivo .env
+// Configure CORS
+app.use(cors({
+  origin: '*', // Permite solicitudes desde este origen
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 
-app.use(cors());
 app.use(express.json());
 
 // Endpoint para toggle del relé
@@ -59,10 +39,7 @@ app.post('/api/riego', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Servidor proxy corriendo en http://localhost:${port}`);
-});
-// Create HTTPS server
-https.createServer(sslOptions, app).listen(CONFIG.port, () => {
-  console.log(`Servidor proxy corriendo en https://localhost:${CONFIG.port}`);
+// Crear y ejecutar el servidor HTTPS
+https.createServer(sslOptions, app).listen(port, () => {
+  console.log(`Servidor proxy corriendo en https://localhost:${port}`);
 });
