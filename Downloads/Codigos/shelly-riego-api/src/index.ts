139181f7babd9971d08d4bc16be179cab2cf09d3
IsaacToledo123 <223245@ids.upchiapas.edu.mx>
2024-07-23 02:32:46 -0600

    llaves

--- Downloads/Codigos/shelly-riego-api/src/index.ts
+++ Downloads/Codigos/shelly-riego-api/src/index.ts
@@ -1,7 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
 import { CONFIG } from './config/config';
 import { controlDeviceOutput } from './Api';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Load SSL certificates from environment variables
+const sslOptions = {
+  key: fs.readFileSync(process.env.SSL_KEY_PATH),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+};
 
 const app = express();
 
@@ -19,6 +31,7 @@ app.post('/api/riego', async (req, res) => {
   }
 });
 
-app.listen(CONFIG.port, () => {
-  console.log(`Servidor proxy corriendo en http://localhost:${CONFIG.port}`);
+// Create HTTPS server
+https.createServer(sslOptions, app).listen(CONFIG.port, () => {
+  console.log(`Servidor proxy corriendo en https://localhost:${CONFIG.port}`);
 });
